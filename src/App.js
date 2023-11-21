75ab02d1eb9cd928ccc205eef2ba15c8965a5aec
yavilar <yavilarobledo@ivytech.edu>
2023-11-21 15:36:14 -0500

    done done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
+import { GoBell, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
 
 function App() {
+    const handleClick=() => {
+        console.log('Click!!');
+    };
     return <div>
+
         <div>
-            <Button>Click me!!</Button>
+            <Button secondary outline rounded className="mb-5" onclick={handleClick}>
+                <GoBell />
+                Click me!!
+            </Button>
         </div>
 
 
         <div>
-            <Button>Buy Now!</Button>
+            <Button danger outline onMouseEnter={handleClick}>
+
+                Buy Now!
+            </Button>
         </div>
 
 
         <div>
-            <Button>See Deal!</Button>
+            <Button warning onMouseLeave={handleClick}>
+                <GoDatabase />
+                See Deal!
+            </Button>
         </div>
 
 
         <div>
-            <Button>Hide Ads!</Button>
+            <Button secondary outline>
+                Hide Ads!
+            </Button>
         </div>
 
 
         <div>
-            <Button>Something!</Button>
+            <Button primary rounded>
+                Something!
+            </Button>
         </div>
 
 
--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types';
+import { twMerge } from 'tailwind-merge';
 
-
+import className from 'classnames';
 
 
 function Button({
@@ -11,10 +11,32 @@ function Button({
     warning,
     danger,
     outline,
-    rounded}) 
+    rounded,
+    ...rest
+
+}) 
 
 {
-    return <button className="bg-red-500">{children}</button>;
+   
+    const classes= twMerge(className(rest.className, 'flex items-center px-3 py-1.5 border',{
+        'border-blue-500 bg-blue-500 text-white':primary,
+        'border-gray-900 bg-gray-900 text-white':secondary,
+        'border-green-500 bg-green-500 text-white':success,
+        'border-yellow-400 bg-yellow-400 text-white':warning,
+        'border-red-500 bg-red-500 text-white':danger,
+        'rounded-full':rounded,
+        'bg-white':outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger,
+
+    })
+    );
+
+   
+    return <button {...rest} className={classes}>{children}</button>;
 }
 
 Button.propTypes = {
